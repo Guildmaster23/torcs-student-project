e8d1bbc09f2003b77dcc091d4e9cb901074da476
Mrat09 <marat09.93@gmail.com>
2017-03-28 21:45:02 +0300

    Corrected race_state handler

--- src/libs/tgfclient/gui.cpp
+++ src/libs/tgfclient/gui.cpp
@@ -436,15 +436,18 @@ static void
 gfuiMotion(int x, int y)
 {
 	GfOut("there a x = %d and y = %d    ", GfuiMouse.X, GfuiMouse.Y);
+	// old values of coordinates of mouse
 	int oldX = GfuiMouse.X;
 	int oldY = GfuiMouse.Y;
+	// new values of coordinates of mouse
 	GfuiMouse.X = (x - (ScrW - ViewW)/2) * (int)GfuiScreen->width / ViewW;
 	GfuiMouse.Y = (ViewH - y + (ScrH - ViewH)/2) * (int)GfuiScreen->height / ViewH;
 	GfOut("there a new x = %d and y = %d\n", GfuiMouse.X, GfuiMouse.Y );
-	//if (ReInfo->_reState == RE_STATE_RACE) {
+	if (ReInfo->_reState == RE_STATE_RACE) {
+		// send diff of coordinates to handler
 		SetRotationFlag((int*)(GfuiMouse.X - oldX),(int*)(GfuiMouse.Y - oldY));
-		
-	//}
+		//GfOut("there a diff x = %d and y = %d\n", (GfuiMouse.X - oldX), (GfuiMouse.Y - oldY) );	
+	}
 	gfuiUpdateFocus();
 	gfuiMouseAction((void*)(1 - GfuiScreen->mouse));
 	glutPostRedisplay();
